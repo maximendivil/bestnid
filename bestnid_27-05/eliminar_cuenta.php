148886db131028d622e188a71f38f8f02d32fca2
aserena <aserena92@gmail.com>
2015-05-27 14:45:00 -0300

    bestnid al 27-05
    Se agrego eliminar usuario por parte del Admin. Cambios en
    eliminar_registrado_db (mas generico) y se usa en eliminar cuenta por
    parte de usuario y admin (para no repetir codigo).

---
+++ bestnid_27-05/eliminar_cuenta.php
@@ -0,0 +1,28 @@
+<?php 
+session_start();
+include("header.php") ; 
+include("cpanel_menu.php");
+
+if(!isset($_SESSION['usuario'])){
+    header("location: index.php");
+}
+?>
+
+<div>
+    
+    <div class="container">
+    	<div class="row">
+		    <h3 style="align-text: center">¿Estás seguro que deseas eliminar tu cuenta?</h3>
+		    <a href="eliminar_registrado_db.php?email=<?php echo $_SESSION['usuario'];?>" style="text-decoration: none"><button class="btn btn-lg btn-warning center-block" style="padding-left: 5% ; padding-right: 5% ; float: left ; margin-right: 20px">Eliminar</button></a>
+		    <a href="javascript:history.back()" style="text-decoration: none"> <button class="btn btn-lg btn-warning center-block" style="padding-left: 5% ; padding-right: 5% ; float: left">Volver</button></a>
+		</div>
+    </div>
+    
+</div>
+
+<div class="clearfix visible-lg"></div>
+	
+</div>
+</div>
+</body>
+</html>
---
+++ bestnid_27-05/eliminar_registrado_db.php
@@ -0,0 +1,21 @@
+<?php
+session_start();
+	$email = $_GET['email'];
+	if( (($_SESSION['tipouser'] == 0) && ($email == $_SESSION['usuario'])) || ($_SESSION['tipouser'] == 1) ){
+            $link = mysql_connect('localhost','root') or die('No se pudo conectar: '.mysql_error());
+            mysql_select_db('bestnid',$link) or die('No se pudo seleccionar la base de datos');
+            mysql_query("UPDATE usuario SET tipo=-1 WHERE email='$email'") or die("Fallo la eliminacion de datos.");
+            mysql_close($link);
+            echo "Cuenta eliminada exitosamente";
+         
+		 if($_SESSION['usuario'] == $email){
+			  header("refresh: 2 ; url = logout.php");
+		 }else if($_SESSION['tipouser'] == 1){
+			header("refresh: 2 ; url = indexPanel.php"); 
+		 }
+	}else{
+		header("Location: index.php");
+	}
+	
+
+?>
